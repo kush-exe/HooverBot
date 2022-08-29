9e0c138ed226c74a0a10a26abdc4d880fac88cd8
Kush <kooshpatel@gmail.com>
2022-08-29 01:48:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { REST, Routes } = require('discord.js');
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
@@ -7,16 +8,43 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
   ]
 });
+//register commands
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
 
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(1013680010273501184), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+//ready
 client.on('ready', () => {
     console.log('Ready');
 })
 
-client.on('message', message => {
-    if (message.content === '$test') {
-        message.reply("Help stepbro I\'m stuck!");
-    }
-})
+//command handlers
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'test') {
+    await interaction.reply('Help stepbro I\'m stuck');
+  }
+});
+
+
 
 //Leave at end
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
